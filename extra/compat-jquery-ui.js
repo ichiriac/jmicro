d57a85320aa8d46303ff9d0e98e97121948a6118
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-08-29 21:53:24 +0200

    starts a new extra compat package for running jquery-ui

---
+++ extra/compat-jquery-ui.js
@@ -0,0 +1,12 @@
+/**
+ * Released under the WTFPL License
+ * Copyright (c) 2013
+ *   Ioan Chiriac
+ *   Anthony Terrien
+ * @link https://github.com/ichiriac/jmicro.js
+ */
+(function($) {
+    // handles the compatibilities to be able to run jquery ui (or plugins)
+    $.expr = {};
+    $.support = {};
+})(jMicro);
