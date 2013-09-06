33c714685bff4f482ba357946baf82a6fe7a25a0
Ioan CHIRIAC <i.chiriac@yahoo.com>
2013-09-06 23:44:28 +0200

    various ajax fixes

--- extra/ajax.js
+++ extra/ajax.js
@@ -10,20 +10,14 @@
                 return new XMLHttpRequest();
             }catch(e) { }
         },
-        send: function(url, method, headers, data, callback) {
+        send: function(url, method, headers, data) {
             var xhr = this.xhr();
+            if (!xhr) throw new Error('Your browser does not handle AJAX');
             xhr.open(method,url,true);
-            xhr.onreadystatechange=function(){
-                if ( callback[xhr.readyState] ) {
-                    callback[xhr.readyState].apply(
-                        xhr, []
-                    );
-                }
-            };
             for(var header in headers) {
-                xhr.setRequestHeader(header,headers);
+                xhr.setRequestHeader(header,headers[header]);
             }
-            xhr.send(data);
+            xhr.send(data ? $.param(data).join('&') : null);
             return xhr;
         }
     };
@@ -32,12 +26,27 @@
             ajax: widget.xhr(),
             cors: 'withCredentials' in widget.xhr()
         },
+        param: function(data, prefix) {
+            var s = [];
+            for( var key in data ) {
+                var value = data[key];
+                if ( typeof value == 'function') value = value();
+                if ( typeof value == 'object') {
+                    s = s.concat(this.param(value, prefix ? prefix + '[' + key + ']' : key));
+                } else {
+                    key = encodeURIComponent(key);
+                    s.push((prefix ? prefix + '[' + key + ']' : key) + '=' + encodeURIComponent(value));
+                }
+            }
+            return s;
+        },
         ajax: function(options) {
             options = $.extend(true, {}, {
                 type: 'GET',
                 url: document.location.href,
                 headers: {
-                    'X-Requested-With': 'XMLHttpRequest'
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'Accept': 'application/json'
                 },
                 success: function() { },
                 data: {}
@@ -45,15 +54,39 @@
             if ( options.type == 'POST' ) {
                 options.headers['Content-type'] = 'application/x-www-form-urlencoded';
             }
-            return widget.send(
+            var xhr = $([widget.send(
                 options.url,
                 options.type,
                 options.headers,
                 options.data,
                 { // ready states :
-                    4: options.success
+                    done: options.success
                 }
-            );
+            )]);
+            xhr[0].onreadystatechange = function() {
+                if ( xhr[0].readyState === 4 ) {
+                    if ( xhr[0].status >= 200 && xhr[0].status < 300 || xhr[0].status === 304 ) {
+                        if ( options.success ) {
+                            options.success.apply(
+                                xhr,
+                                [JSON.parse(xhr[0].responseText), xhr[0]]
+                            );
+                        }
+                        xhr.trigger('success');
+                    } else {
+                        if ( options.error ) {
+                            options.error.apply(
+                                xhr,
+                                [xhr[0], xhr[0].status, xhr[0].statusText]
+                            );
+                        }
+                        xhr.trigger('error');
+                    }
+                }
+            };
+            return xhr.on('readystatechange', function() {
+                $(this).trigger('ajaxComplete');
+            });
         }
     });
 })(jMicro);
